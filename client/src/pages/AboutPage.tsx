3352210ca69b685b9952ed3dd51738d578c3a3c0
JORT <jordan.unzaga22@gmail.com>
2025-12-02 13:51:24 -0700

    updated the About page B)

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -12,14 +12,15 @@ export default function AboutPage() {
                 <h1>About DrinkSync</h1>
 
                 <p>
-                    A web app where you can be suggested random mixed drinks, look up recipes you’re curious about, and save recipes you enjoy!
+                    A web app where you are suggested random mixed drinks, look up recipes you’re curious about, and save recipes you enjoy!
                 </p>
 
                 <h2>Features</h2>
                 <ul>
                     <li>Browse drinks fetched live from CocktailDB API</li>
                     <li>Detailed drink pages with ingredients + instructions</li>
-                    <li>Planned: user accounts and saved drinks</li>
+                    <li>User accounts and Savable drinks</li>
+                    <li>Infinite Scroll!</li>
                 </ul>
 
                 <h2>Technology</h2>
@@ -27,6 +28,7 @@ export default function AboutPage() {
                     <li>React + TypeScript (frontend)</li>
                     <li>React Router for navigation</li>
                     <li>Custom CSS for styling</li>
+                    <li>MariaDB and PHP</li>
                 </ul>
             </div>
         </>
